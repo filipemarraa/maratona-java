35e56a69b307f62527c5a6089e8e5c48188f9d82
filipemarraa <filipejacobsonmarra@gmail.com>
2024-08-26 21:09:57 -0300

    Novas aulas da maratona Java Virado no Jiraya

---
+++ src/academia/filipe/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package academia.filipe.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        // int, double, float, char, byte, short, long, boolean
+        int ageForDad = 10;
+        System.out.println(ageForDad);
+        System.out.println("ageForDad");
+        System.out.println("A idade do pai é " + ageForDad + " anos");
+    }
+}
---
+++ src/academia/filipe/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,12 @@
+package academia.filipe.maratonajava.introducao;
+
+/**
+ * javadoc
+ * Famoso por documentar o codigo/classe a baixo
+ */
+
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
