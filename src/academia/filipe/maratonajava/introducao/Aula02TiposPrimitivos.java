88c168bb3e83c17dca859db3f034f500e816a7ed
filipemarraa <filipejacobsonmarra@gmail.com>
2024-08-28 16:45:37 -0300

    Novos ensinamentos da maratona Java Virado no Jiraya

--- src/academia/filipe/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academia/filipe/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -8,5 +8,17 @@ public class Aula02TiposPrimitivos {
         System.out.println(ageForDad);
         System.out.println("ageForDad");
         System.out.println("A idade do pai é " + ageForDad + " anos");
+
+        int age = 19;
+        long numberGrande = 100000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caracter = 'M'; //Apenas uma letra (Bom pra usar em Masculino(M) e Feminino(F))
+
+        System.out.println("O seu pai é " + falso);
     }
 }
