f8aaeb208a93e3b45d628eb5a6d285adfcb4db3c
filipemarraa <filipejacobsonmarra@gmail.com>
2024-11-19 19:09:44 -0300

    Aulas novas do DevDojo

---
+++ src/java10x/condicoes/Array.java
@@ -0,0 +1,31 @@
+package java10x.condicoes;
+
+public class Array {
+    public static void main(String[] args) {
+
+        //Arrays sao tipo referencia!
+        //String inicia como NULL!
+
+        //Arrays sao tipos
+        String [] nomes = new String[5];
+        nomes[0] = "Filipe";
+        nomes[1] = "jacobson";
+        nomes[2] = "marra";
+        nomes[3] = "jeferson";
+        nomes[4] = "tavarez";
+
+        //Array de idade
+        int [] idade = new int[2];
+        System.out.println(idade); // Local da memoria ques está alocado o ARRAY
+
+        boolean [] testando = new boolean[2];
+        System.out.println(testando[1]);
+
+
+
+        System.out.println("Nome que estou falando: " +nomes[4]);
+
+    }
+
+}
+
